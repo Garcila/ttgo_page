be6c9e690d420ce5444ce6c270e6b898fe2a959f
German Arcila <germanarcila@gmail.com>
2018-05-10 11:03:35 -0400

    added footer

--- script.js
+++ script.js
@@ -4,15 +4,40 @@ const root = document.getElementById('root');
 // Create elements in page.  Title, subtitle, paragraph
 const title = document.createElement('div');
 title.setAttribute('class', 'title');
+
 const logo = document.createElement('img');
 logo.setAttribute('class', 'logo');
+
 const subtitle = document.createElement('h2');
 const paragraph = document.createElement('p');
+
+const footer = document.createElement('div');
+footer.setAttribute('class', 'footer');
 
 logo.src = './images/TTG.png'
 subtitle.textContent = 'Season 2 list of episodes';
 paragraph.textContent =
 	'This is a list of all of the episodes of Teen Titans Go as presented by IMDb. You may see more of this content at "https://www.imdb.com/title/tt2771780/episodes?season=2".';
+
+const robin = document.createElement('img');
+robin.setAttribute('class', 'robin');
+robin.src = './images/robin3.png';
+
+const raven = document.createElement('img');
+raven.setAttribute('class', 'raven');
+raven.src = './images/raven2.png';
+
+const cyborg = document.createElement('img');
+cyborg.setAttribute('class', 'cyborg');
+cyborg.src = './images/cyborg2.png';
+
+const starfire = document.createElement('img');
+starfire.setAttribute('class', 'starfire');
+starfire.src = './images/starfire.png';
+
+const beastboy = document.createElement('img');
+beastboy.setAttribute('class', 'beast-boy');
+beastboy.src = './images/beast-boy.png';
 
 title.appendChild(logo);
 root.appendChild(title);
@@ -60,4 +85,12 @@ fetch('https://ttgoapi.herokuapp.com')
 			card.appendChild(description);
 		})
 	)
-	.catch(error => console.log(error));
+  .catch(error => console.log(error));
+  
+  root.appendChild(footer);
+
+footer.appendChild(robin);
+footer.appendChild(beastboy);
+footer.appendChild(starfire);
+footer.appendChild(cyborg);
+footer.appendChild(raven);
